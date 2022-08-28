bae84d7eba860d2ede744fa51362cac1663ed2cf
PabloZavi <pablozaviok@gmail.com>
2022-08-28 13:05:24 -0300

    Subo TP2 Parte1

---
+++ Controlador.cs
@@ -0,0 +1,143 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Controlador : Conexion
+    {
+        public bool existePais(int codigoPais)
+        {
+            bool existe = false;
+
+            try
+            {
+                MySqlDataReader reader;
+                String sql = "Select * from tp2laboratorio4.pais WHERE codigoPais=" + codigoPais + ";";
+
+                MySqlConnection conexionBD = base.conexion();
+                conexionBD.Open();
+                MySqlCommand comando = new MySqlCommand(sql, conexionBD);
+                reader = comando.ExecuteReader();
+
+                if (reader.Read()) existe = true;
+                else existe = false;
+
+                conexionBD.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+
+            return existe;
+        }
+
+        public void actualizarPais(long codigoPais, string nombrePais, string capitalPais, string regionPais, long poblacion, double latitud, double longitud)
+        {
+            try
+            {
+                //MySqlDataReader reader;
+                //String sql = "Select * from tp2laboratorio4.pais WHERE codigoPais='" + nombrePais + "'";
+                //String sql = "UPDATE tp2laboratorio4.pais SET nombrePais = '" + nombrePais +
+                //    "', capitalPais = '" + capitalPais +
+                //    "', region = '" + regionPais +
+                //    "', poblacion = " + poblacion +
+                //    ", latitud = " + latitud +
+                //    ", longitud = " + longitud +
+                //    " WHERE codigoPais = " + codigoPais + ";";
+
+                //Con el método anterior, cuando las latitudes y/o longitudes tenían decimales, se convertían los puntos en comas
+                //al hacer el update a sql y por ende no se podía realizar, por lo que para evitar estos inconvenientes
+                //lo mejor es parametrizar de la siguiente forma
+
+                String sql = "UPDATE tp2laboratorio4.pais SET nombrePais = @nombre, capitalPais = @capital," +
+                    " region = @region, poblacion = @poblacion, latitud = @latitud, longitud = @longitud " +
+                    "WHERE codigoPais = @codigo;";
+
+                MySqlConnection conexionBD = base.conexion();
+                conexionBD.Open();
+                MySqlCommand comando = new MySqlCommand(sql, conexionBD);
+
+                MySqlParameter parCodigo = comando.Parameters.Add("@codigo", MySqlDbType.Int32);
+                MySqlParameter parNombre = comando.Parameters.Add("@nombre", MySqlDbType.String);
+                MySqlParameter parCapital = comando.Parameters.Add("@capital", MySqlDbType.String);
+                MySqlParameter parRegion = comando.Parameters.Add("@region", MySqlDbType.String);
+                MySqlParameter parPoblacion = comando.Parameters.Add("@poblacion", MySqlDbType.Int32);
+                MySqlParameter parLatitud = comando.Parameters.Add("@latitud", MySqlDbType.Double);
+                MySqlParameter parLongitud = comando.Parameters.Add("@longitud", MySqlDbType.Double);
+                parCodigo.Value = codigoPais;
+                parNombre.Value = nombrePais;
+                parCapital.Value = capitalPais;
+                parRegion.Value = regionPais;
+                parPoblacion.Value = poblacion;
+                parLatitud.Value = latitud;
+                parLongitud.Value = longitud;
+
+                comando.ExecuteNonQuery();
+
+                conexionBD.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+
+        public void insertarPais(int codigoPais, string nombrePais, string capitalPais, string regionPais, long poblacion, double latitud, double longitud)
+        {
+            try
+            {
+                //MySqlDataReader reader;
+                //String sql = "Select * from tp2laboratorio4.pais WHERE codigoPais='" + nombrePais + "'";
+                //String sql = "insert into tp2laboratorio4.pais(codigoPais, nombrePais, capitalPais, region, poblacion, latitud, longitud) values ("
+                //    + codigoPais + ","
+                //    + "'" + nombrePais + "',"
+                //    + "'" + capitalPais + "',"
+                //    + "'" + regionPais + "',"
+                //    + poblacion + ","
+                //    + latitud + ","
+                //    + longitud + ");";
+
+                //Con el método anterior, cuando las latitudes y/o longitudes tenían decimales, se convertían los puntos en comas
+                //al hacer el update a sql y por ende no se podía realizar, por lo que para evitar estos inconvenientes
+                //lo mejor es parametrizar de la siguiente forma
+
+                String sql = "insert into tp2laboratorio4.pais(codigoPais, nombrePais, capitalPais, region, poblacion, latitud, longitud) " +
+                    "values (@codigo, @nombre, @capital, @region, @poblacion, @latitud, @longitud);";
+                    
+                MySqlConnection conexionBD = base.conexion();
+                conexionBD.Open();
+                MySqlCommand comando = new MySqlCommand(sql, conexionBD);
+                MySqlParameter parCodigo = comando.Parameters.Add("@codigo", MySqlDbType.Int32);
+                MySqlParameter parNombre = comando.Parameters.Add("@nombre", MySqlDbType.String);
+                MySqlParameter parCapital = comando.Parameters.Add("@capital", MySqlDbType.String);
+                MySqlParameter parRegion = comando.Parameters.Add("@region", MySqlDbType.String);
+                MySqlParameter parPoblacion = comando.Parameters.Add("@poblacion", MySqlDbType.Int32);
+                MySqlParameter parLatitud = comando.Parameters.Add("@latitud", MySqlDbType.Double);
+                MySqlParameter parLongitud = comando.Parameters.Add("@longitud", MySqlDbType.Double);
+                parCodigo.Value = codigoPais;
+                parNombre.Value = nombrePais;
+                parCapital.Value = capitalPais;
+                parRegion.Value = regionPais;
+                parPoblacion.Value = poblacion;
+                parLatitud.Value = latitud;
+                parLongitud.Value = longitud;
+                comando.ExecuteNonQuery();
+
+                conexionBD.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Primero sé que voy a recibir datos de una URL que tiene datos Json, en este caso:
+            //"https://restcountries.com/v2/callingcode/{callingcode}
+
+            //1. Lo que tengo que hacer, es buscar cualquier código de ejemplo que nos dará 
+            //   el Json de dicho país.
+            //2. Copiar el Json e ir a https://json2csharp.com/ que nos convertirá el Json a una clase
+            //   de C#, copiamos esa clase y creamos una clase en C# llamada (en este caso) Pais
+            //   y le pegamos lo que nos devolvió la página. Ojo! Tendrá como nombre "Root" en el código, 
+            //   por lo que lo tendremos que cambiar a "Pais". Y puede que haya problemas con los atributos
+            //   de tipo double, por lo que hay que cambiarlos a string.
+
+            //Ahora empezaremos a recibir los datos (de a uno) de los primeros 300 países
+            for (int i = 1; i <= 300; i++)
+            {
+                String url = "https://restcountries.com/v2/callingcode/" + i;
+                //Uso la clase WebClient que pasa la url a string...
+                WebClient wc = new WebClient();
+                //con el método DownLoadString:
+
+                //Console.WriteLine(paisJson);
+
+                //Ahora tengo los datos almacenados de un país en un string
+                //Lo que tenemos que hacer es deserializar ese Json en un objeto C#
+                //Primero tenemos que instalar una dependencia para poder usar el método "JsonConvert":
+                //Clic derecho en "Dependencias" --> "Administrar paquetes NuGet" --> Buscar e instalar "NewtonSoft.Json"
+                //Ahora sí convierte el paísJson a un objeto Pais
+                //(Creamos una variable de tipo convert, deserializamos con: JsonConvert.DeserializeObject<NombreClase>(string que tiene el Json)
+
+                try
+                {
+                    var paisJson = wc.DownloadString(url);
+                    //No sé por qué hay que crear un <List> de Pais para que funcione siendo que es un solo país
+                    //pero se hace así y se accede a la posición 0
+                    //var pais = JsonConvert.DeserializeObject<Pais>(paisJson);
+                    var pais = JsonConvert.DeserializeObject<List<Pais>>(paisJson);
+                    //Pais paiss = (Pais)pais;
+                    //Console.WriteLine(pais[0].callingCodes[0]);
+                    //Console.WriteLine(pais[0].name);
+                    Controlador contr = new Controlador();
+                    if (contr.existePais(Convert.ToInt32(pais[0].callingCodes[0])))
+                    {
+                        contr.actualizarPais(Convert.ToInt32(pais[0].callingCodes[0]), pais[0].name, pais[0].capital,
+                            pais[0].region, pais[0].population, pais[0].latlng[0], pais[0].latlng[1]);
+                    }
+                    else
+                    {
+                        contr.insertarPais(Convert.ToInt32(pais[0].callingCodes[0]), pais[0].name, pais[0].capital,
+                            pais[0].region, pais[0].population, pais[0].latlng[0], pais[0].latlng[1]);
+                        Console.WriteLine(pais[0].latlng[0]);
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message.ToString());
+
+                }
+
+
+
+                //Otra forma de hacerlo es directamente deserializar el string que me trae el método WebClient sin tener
+                //que guardarlo en una variable String.
+                //Pais pais2 = JsonConvert.DeserializeObject<Pais>(wc.DownloadString(url));
+
+                //Recordar que tenemos que crear una clase "Conexion" para comunicarnos con la base de datos
+                //y que tenemos que agregar la referencia a MySql (clic derecho en Dependencias-->Agregar referencia del proyecto
+                //y buscar el conector a mysql)
+
+
+
+            }
+
+
+
+
+
+        }
+    }
+}
